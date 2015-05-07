2455509e4efce93da263cf33542cfb7e46e50b67
kata <kata.masa@gmail.com>
2015-05-07 23:53:54 +0900

    change brew file -> brew-file

--- bin/brewcaskupdate.sh
+++ bin/brewcaskupdate.sh
@@ -4,6 +4,7 @@ LOG="/Users/kata/log/`date "+%Y%m%d"`.log"
 
 brew doctor >> ${LOG} 2>&1
 if [ $? -ne 0 ]; then
-	brew file update >> ${LOG} 2>&1
-	brew file cask_upgrade -C >> ${LOG} 2>&1
+	brew update >> ${LOG} 2>&1
+	brew-file update >> ${LOG} 2>&1
+	brew-file cask_upgrade -C >> ${LOG} 2>&1
 fi
